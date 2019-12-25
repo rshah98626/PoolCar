7f0c69efc240819209d5ae7584f55fff308e4dc5
Rahul Shah <rshah98626@gmail.com>
2019-12-25 17:56:57 -0500

    Initial Commit

---
+++ PoolCar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PoolCar
+//
+//  Created by Rahul Shah on 12/25/19.
+//  Copyright © 2019 RSInc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
