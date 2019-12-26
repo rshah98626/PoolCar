c9e7d317fe136941843544462f6b4c48e34f634a
Rahul Shah <rshah98626@gmail.com>
2019-12-25 21:45:08 -0500

    Initialized row

---
+++ PoolCar/RideRow.swift
@@ -0,0 +1,27 @@
+//
+//  RideRow.swift
+//  PoolCar
+//
+//  Created by Rahul Shah on 12/25/19.
+//  Copyright © 2019 RSInc. All rights reserved.
+//
+
+import SwiftUI
+
+struct RideRow: View {
+    var ride: Ride
+    var body: some View {
+        HStack {
+            Text(ride.destination)
+            Text(ride.origin)
+            Spacer()
+        }.padding()
+    }
+}
+
+struct RideRow_Previews: PreviewProvider {
+    static var previews: some View {
+        RideRow(ride: tempRides[0])
+            .previewLayout(.fixed(width: 300, height: 50))
+    }
+}
