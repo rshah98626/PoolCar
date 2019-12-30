4737f919ea1adf532866eb0ac3c6582f680ca334
Rahul Shah <rshah98626@gmail.com>
2019-12-30 14:43:41 -0500

    Refactored ride table

--- PoolCar/Ride Table/RideTable.swift
+++ PoolCar/Ride Table/RideTable.swift
@@ -1,8 +1,8 @@
 //
-//  ContentView.swift
+//  RideTable.swift
 //  PoolCar
 //
-//  Created by Rahul Shah on 12/25/19.
+//  Created by Rahul Shah on 12/30/19.
 //  Copyright © 2019 RSInc. All rights reserved.
 //
 
@@ -10,73 +10,12 @@ import SwiftUI
 
 struct RideTable: View {
     @EnvironmentObject var database: Database
-    @State private var showAddRideModal: Bool = false
-    @State private var drawerOpen: Bool = false
-
-    /// Button to add ride
-    var addRideButton: some View {
-        HStack {
-            Button(action: {
-                self.showAddRideModal.toggle()
-            }) {
-                Image(systemName: "car.fill")
-                    .font(.largeTitle)
-            }
-            .foregroundColor(.blue)
-        }
-    }
-
-    /// Menu drawer button
-    var sideMenuButton: some View {
-        HStack {
-            Button(action: {
-                // background thread
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-//                    self.drawerOpen.toggle()
-//                }
-                self.drawerOpen.toggle()
-            }) {
-                Image(systemName: "sidebar.left")
-                    .font(.largeTitle)
-            }
-            .foregroundColor(.blue)
-        }
-    }
-
     var body: some View {
-        ZStack {
-            NavigationView {
-                // Ride table
-                List(database.rides) { ride in
-                    NavigationLink(destination: RideDetail(ride: ride)) {
-                        RideRow(ride: ride)
-                    }
-                }
-                .navigationBarTitle("Rides", displayMode: .inline)
-                .navigationBarItems(leading: sideMenuButton, trailing: addRideButton)
-                .sheet(isPresented: $showAddRideModal) {
-                    AddRide(isShowing: self.$showAddRideModal)
-                        .environmentObject(self.database)
-                }
+        List(database.rides) { ride in
+            NavigationLink(destination: RideDetail(ride: ride)) {
+                RideRow(ride: ride)
             }
-
-            // side drawer
-            NavigationDrawer(isOpen: self.$drawerOpen)
         }
-    }
-
-    init() {
-        // navigation bar color
-        UINavigationBar.appearance().backgroundColor = .green
-
-        // for displayMode .large
-        UINavigationBar.appearance().largeTitleTextAttributes = [
-            .foregroundColor: UIColor.darkGray,
-            .font: UIFont(name: "Papyrus", size: 40)!]
-
-        // for displayMode .inline
-        UINavigationBar.appearance().titleTextAttributes = [
-            .font: UIFont(name: "HelveticaNeue-Thin", size: 20)!]
     }
 }
 
