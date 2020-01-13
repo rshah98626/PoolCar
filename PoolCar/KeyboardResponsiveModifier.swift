3a4a2091e305832d00d842dfb3c7f4830a82effa
Rahul Shah <rshah98626@gmail.com>
2020-01-12 20:21:47 -0400

    Made spacing changes

--- PoolCar/KeyboardResponsiveModifier.swift
+++ PoolCar/KeyboardResponsiveModifier.swift
@@ -11,33 +11,31 @@
 import SwiftUI
 
 struct KeyboardResponsiveModifier: ViewModifier {
-  @State private var offset: CGFloat = 0
+    @State private var offset: CGFloat = 0
 
-  func body(content: Content) -> some View {
-    content
-      .padding(.bottom, offset)
-      .onAppear {
-        NotificationCenter.default.addObserver(forName:
-            UIResponder.keyboardWillShowNotification, object: nil,
-            queue: .main) { notif in
-          let value = notif.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
-          let height = value!.height
-          let bottomInset = UIApplication.shared.windows.first?.safeAreaInsets.bottom
-          self.offset = height - (bottomInset ?? 0)
-        }
+    func body(content: Content) -> some View {
+        content
+            .padding(.bottom, offset)
+            .onAppear {
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification,
+                    object: nil, queue: .main) { notif in
+                        let value = notif.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+                        let height = value!.height
+                        let bottomInset = UIApplication.shared.windows.first?.safeAreaInsets.bottom
+                        self.offset = height - (bottomInset ?? 0)
+                }
 
-        NotificationCenter.default.addObserver(forName:
-            UIResponder.keyboardWillHideNotification, object: nil,
-            queue: .main) { _ in
-          self.offset = 0
-        }
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification,
+                    object: nil, queue: .main) { _ in
+                        self.offset = 0
+                }
+            }
+            .animation(.spring())
     }
-    .animation(.spring())
-  }
 }
 
 extension View {
-  func keyboardResponsive() -> ModifiedContent<Self, KeyboardResponsiveModifier> {
-    return modifier(KeyboardResponsiveModifier())
-  }
+    func keyboardResponsive() -> ModifiedContent<Self, KeyboardResponsiveModifier> {
+        return modifier(KeyboardResponsiveModifier())
+    }
 }
