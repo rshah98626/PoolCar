2d04e30df6e8031b94762ad878a4d0ea723ba990
Raajesh12 <raajstar3@gmail.com>
2020-02-12 13:31:34 -0600

    Code to get rides and update UI with them

--- PoolCar/Ride Table/RideTable.swift
+++ PoolCar/Ride Table/RideTable.swift
@@ -7,15 +7,50 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct RideTable: View {
     @EnvironmentObject var database: Database
+    @State var rides: [Ride] = [Ride]()
+    
     var body: some View {
-        List(database.rides) { ride in
-            // Navigation Link makes text and other items look faint in preview
-            NavigationLink(destination: RideDetail(ride: ride)) {
-                RideRow(ride: ride)
+        VStack {
+            if rides.isEmpty {
+                List(database.rides) { ride in
+                    // Navigation Link makes text and other items look faint in preview
+                    NavigationLink(destination: RideDetail(ride: ride)) {
+                        RideRow(ride: ride)
+                    }
+                }
+            } else {
+                List(rides) { ride in
+                   // Navigation Link makes text and other items look faint in preview
+                   NavigationLink(destination: RideDetail(ride: ride)) {
+                       RideRow(ride: ride)
+                   }
+                }
             }
+        }
+        .onAppear() {
+            self.GetRides()
+        }
+    }
+    
+    func GetRides() {
+        //node URL
+        let url = "https://infinite-stream-52265.herokuapp.com/rides/getAll"
+        
+        AF.request(url, method: .get, headers: NetworkingUtilities.getAuthorizationHeaders())
+            .validate()
+            .responseData() { response in
+                switch response.result {
+                case let .success(data):
+                    let decoder = JSONDecoder()
+                    let ridesServer = try? decoder.decode([Ride].self, from: data)
+                    self.rides = ridesServer ?? [Ride]()
+                case let .failure(error):
+                    print(error)
+                }
         }
     }
 }
