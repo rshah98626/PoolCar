8debf184a947139f58481e20ed9a76213e01784a
Rahul Shah <rshah98626@gmail.com>
2020-02-12 18:08:13 -0600

    Fixed minor tabbing issues

--- PoolCar/SignupView.swift
+++ PoolCar/SignupView.swift
@@ -18,46 +18,46 @@ struct SignupView: View {
     var body: some View {
         VStack{
             if signedUp == 0{
-       VStack {
-            Text("Create a New User")
-                .font(.title)
-                .fontWeight(.bold)
-
-            Text("please enter your info")
-                .font(.subheadline)
-                .fontWeight(.light)
-
-        //name text field - needs proper formatting
-            TextField("Name", text: $name)
-            .padding(.horizontal)
-            .frame(width: 200.0, height: 30.0)
-            .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
-            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-        
-            //email field - needs proper formatting
-            TextField("Email", text: $email)
-                .padding(.horizontal)
-                .frame(width: 200.0, height: 30.0)
-                .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
-                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-        
-            //password field, is set to secure typing
-            SecureField("Password", text: $passwrd)
-            .padding(.horizontal)
-            .frame(width: 200.0, height: 30.0)
-            .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
-            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-  
-            
-        //button handle calling AF and submitting entered information - NEEDS to reject when fields are not entered
-        Button(action: {self.SignupRequest(email: self.email, pass: self.passwrd, name: self.name)}) {
-            Text("Sign Up")
-                .frame(width: nil)
-        }
-            }}
+                VStack {
+                    Text("Create a New User")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("please enter your info")
+                        .font(.subheadline)
+                        .fontWeight(.light)
+                    
+                    //name text field - needs proper formatting
+                    TextField("Name", text: $name)
+                        .padding(.horizontal)
+                        .frame(width: 200.0, height: 30.0)
+                        .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    
+                    //email field - needs proper formatting
+                    TextField("Email", text: $email)
+                        .padding(.horizontal)
+                        .frame(width: 200.0, height: 30.0)
+                        .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    
+                    //password field, is set to secure typing
+                    SecureField("Password", text: $passwrd)
+                        .padding(.horizontal)
+                        .frame(width: 200.0, height: 30.0)
+                        .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    //button handle calling AF and submitting entered information - NEEDS to reject when fields are not entered
+                    Button(action: {self.SignupRequest(email: self.email, pass: self.passwrd, name: self.name)}) {
+                        Text("Sign Up")
+                            .frame(width: nil)
+                    }
+                }}
             else{
                 //takes user to the home page once successful signup
-
+                
                 Home()
             }
         }
@@ -68,7 +68,7 @@ struct SignupView: View {
         //node URL
         let url = "https://infinite-stream-52265.herokuapp.com/users/signup"
         let signup = Signup(name: name, email: email, password: pass)
-
+        
         AF.request(url, method: .post, parameters: signup)
             .validate()
             .responseString { response in
@@ -79,7 +79,7 @@ struct SignupView: View {
                 case let .failure(error):
                     print(error)
                     
-            }
+                }
         }
     }
 }
